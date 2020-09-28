68d975a81e1e1de66407901d8a6ae2980f015d15
NinjaJJ <jason.k.lo1209@gmail.com>
2020-09-28 10:07:51 -0500

    != > equal

--- src/main/java/com/infosys/timetracker/eim/controller/GroupController.java
+++ src/main/java/com/infosys/timetracker/eim/controller/GroupController.java
@@ -38,7 +38,7 @@ public class GroupController {
 
     @PutMapping("/{id}")
     public void updateGroup(@PathVariable String id, @RequestBody Group group) {
-        if (group.getGroupId()!=(id)) {
+        if (!group.getGroupId().equals(id)) {
             throw new IllegalStateException("Given group's ID doesn't match the ID in the path.");
         }
         groupRepo.save(group);
